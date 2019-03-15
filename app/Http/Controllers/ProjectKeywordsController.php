0be60893bfb06be75a2374d7dae9b576f9c9bf46
Farscape <geoligard@gmail.com>
2019-03-15 14:53:21 +0100

    - Sort .readme file and minor code cleanups

--- app/Http/Controllers/ProjectKeywordsController.php
+++ app/Http/Controllers/ProjectKeywordsController.php
@@ -35,7 +35,7 @@ class ProjectKeywordsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
         $this->validate($request, [
             'keywords' => 'required'
         ]);
